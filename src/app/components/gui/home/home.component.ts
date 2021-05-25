16285bf8a6c24644664033ebca008fa7103d0652
root <root@hb57249flex.ikexpress.com>
2021-05-25 08:47:08 +0000

    first

--- src/app/components/gui/home/home.component.ts
+++ src/app/components/gui/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'node-home',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  refApiImage = environment.api_image;
   constructor() { }
 
   ngOnInit(): void {
