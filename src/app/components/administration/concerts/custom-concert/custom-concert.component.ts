1519555a3afbc3633e34efc3946f217132129ba2
root <root@hb57249flex.ikexpress.com>
2021-06-29 08:02:47 +0000

    29/06 - Gestion des concerts

--- src/app/components/administration/concerts/custom-concert/custom-concert.component.ts
+++ src/app/components/administration/concerts/custom-concert/custom-concert.component.ts
@@ -12,6 +12,7 @@ export class CustomConcertComponent implements OnInit {
   @Input() concertForm :  FormGroup;
   @Input() imagePreview : string;
   evenement: Evenement;
+
   constructor() { }
 
   ngOnInit(): void {
